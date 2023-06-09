96ea7f3aa9b2aef90c10d679f337beb5b90e498c
vladevpro <112557148+vladevpro@users.noreply.github.com>
2023-06-09 07:26:02 +0300

    Update preview.ex

--- preview.ex
+++ preview.ex
@@ -1,6 +1,9 @@
 defmodule GameAdminFlaskWeb.Components.CategoryGroups.Preview do
   use GameAdminFlaskWeb, :stateful_component
 
+  alias GameAdminFlask.Actions.CreateOrUpdateCategoryGroup
+  alias Moon.Assets.Icons.IconArrowDown
+  alias Moon.Assets.Icons.IconArrowUp
   alias Moon.Assets.Icons.IconCloseSmall
   alias Moon.Assets.Icons.IconPencil
   alias Moon.Autolayouts.LeftToRight
@@ -9,7 +12,6 @@ defmodule GameAdminFlaskWeb.Components.CategoryGroups.Preview do
   alias Moon.Components.Button
   alias Moon.Components.Deprecated.TextInput
   alias Moon.Components.ErrorTag
-  alias Moon.Components.Select.MultiSelect
   alias Moon.Components.SlideOver
   alias Moon.Design.Form
 
@@ -28,30 +30,63 @@ defmodule GameAdminFlaskWeb.Components.CategoryGroups.Preview do
   prop(on_submit, :event, required: true)
   prop(on_select, :event)
 
-  prop(selected_games, :list, default: [])
-  prop(selected, :list, default: [])
-  prop(selected_game_indexes, :list, default: [])
+  data(selected_games, :any)
+  data(selected_categories, :any)
 
-  def mount(_params, _session, socket) do
+  def update(updates, socket) do
+    socket =
+      socket
+      |> assign(updates)
+
+    %{games: games, categories: categories, changeset: changeset} = socket.assigns
+
+    # Handling game_ids.
+    game_ids =
+      case changeset do
+        %{changes: %{game_ids: game_ids}} ->
+          game_ids
+
+        _ ->
+          []
+      end
+
+    selected_games =
+      game_ids
+      |> Enum.map(fn game_id -> Enum.find(games, &(&1.value == to_string(game_id))) end)
+      |> Enum.reject(&is_nil/1)
+
+    # Handling category_ids.
+    category_ids =
+      case changeset do
+        %{changes: %{category_ids: category_ids}} ->
+          category_ids
+
+        _ ->
+          []
+      end
+
+    selected_categories =
+      category_ids
+      |> Enum.map(fn category_id -> Enum.find(categories, &(&1.value == to_string(category_id))) end)
+      |> Enum.reject(&is_nil/1)
+
     socket =
       assign(socket,
-        selected: [],
-        selected_games: []
+        selected_games: selected_games,
+        selected_categories: selected_categories
       )
 
     {:ok, socket}
   end
 
   def render(assigns) do
-    IO.inspect(assigns.selected_games, label: "Selected Games in render")
-    IO.inspect(assigns.selected, label: "Selected in render")
     ~F"""
     <SlideOver on_close={@on_close}>
       <:heading>Update for <strong>{@uid}</strong>
       </:heading>
       <:content>
         <LeftToRight>
-          <Form id="update_category_group_form" class="flex-1" for={@changeset} change={@on_change}>
+          <Form id="update_category_group_form" class="flex flex-col flex-1 gap-3" for={@changeset} change={@on_change}>
             <LeftToRight>
               <Form.Field class="basis-2/4 mb-2" field={:site_id} label="Site">
                 <Form.Select options={@sites} prompt="Select Site" class="w-full bg-goku" size="lg" disabled />
@@ -69,9 +104,10 @@ defmodule GameAdminFlaskWeb.Components.CategoryGroups.Preview do
               </Form.Field>
             </LeftToRight>
 
-            <!-- NEW TEST DROPDOWN FORM -->
-            <Form.Field field={:game_ids} label="Games" hint="Select games" class= "opacity-1">
+            <!-- Games DROPDOWN FORM -->
+            <Form.Field field={:game_ids} label="Games"  class= "opacity-1">
               <Form.Dropdown
+                id="game_dropdown"
                 is_multiple
                 options={Enum.map(@games, &%{key: &1.label, value: String.to_integer(&1.value)})}
                 prompt="Select Games"
@@ -79,111 +115,194 @@ defmodule GameAdminFlaskWeb.Components.CategoryGroups.Preview do
               />
             </Form.Field>
 
-            <Form.Field class="basis-2/4 mb-2" field={:category_ids} label="Categories">
-              <MultiSelect
-                id="category_ids"
-                options={@categories}
+            <!-- Games TABLE -->
+            <MoonTable id="game_table" items={game <- @selected_games}>
+              <Column name="actions" label="Actions">
+                  <LeftToRight>
+                  <Button on_click={move_up_event(game.id)} disabled={game.value == hd(@selected_games).value}>
+                  <:left_icon_slot><IconArrowUp /></:left_icon_slot>
+                  </Button>
+                  <Button on_click={move_down_event(game.id)} disabled={game.value == List.last(@selected_games).value}>
+                  <:left_icon_slot><IconArrowDown /></:left_icon_slot>
+                  </Button>
+                </LeftToRight>
+              </Column>
+              <Column name="value" label="Game Value">
+                {game.value}
+              </Column>
+              <Column name="label" label="Game Label">
+                {game.label}
+              </Column>
+            </MoonTable>
+
+            <!-- Categories DROPDOWN FORM -->
+              <Form.Field field={:category_ids} label="Categories"  class= "opacity-1">
+              <Form.Dropdown
+                id="category_dropdown"
+                is_multiple
+                options={Enum.map(@categories, &%{key: &1.label, value: String.to_integer(&1.value)})}
                 prompt="Pick Categories"
-                selected_label_text_color_class="text-goten"
+                is_open={Map.get(assigns, :target, []) === ["create_or_update_category_group", "category_ids"]}
               />
-              <ErrorTag />
             </Form.Field>
+
+            <!-- Categories TABLE -->
+            <MoonTable id="category_table" items={category <- @selected_categories}>
+              <Column name="actions" label="Actions">
+                  <LeftToRight>
+                  <Button on_click={move_up_event_category(category.id)} disabled={category.value == hd(@selected_categories).value}>
+                  <:left_icon_slot><IconArrowUp /></:left_icon_slot>
+                  </Button>
+                  <Button on_click={move_down_event_category(category.id)} disabled={category.value == List.last(@selected_categories).value}>
+                  <:left_icon_slot><IconArrowDown /></:left_icon_slot>
+                  </Button>
+                </LeftToRight>
+              </Column>
+              <Column name="value" label="Category Value">
+                {category.value}
+              </Column>
+              <Column name="label" label="Category Label">
+                {category.label}
+              </Column>
+            </MoonTable>
+
           </Form>
         </LeftToRight>
+
         <PullAside class="mt-8">
-          <:right>
-            <LeftToRight>
-              <Button variant="secondary" on_click={@on_close}>
-                <:left_icon_slot><IconCloseSmall /></:left_icon_slot>
-                Close
-              </Button>
-              <Button on_click={@on_submit} disabled={!@changeset.valid?}>
-                <:left_icon_slot><IconPencil /></:left_icon_slot>
-                Update
-              </Button>
-            </LeftToRight>
-          </:right>
-        </PullAside>
-
-        <!-- MOON TABLE -->
-        <MoonTable items={game <- @selected_games} row_click="single_row_click" {=@selected}>
-          <Column name="actions" label="Actions">
-            <Button {:on_click, move_up_event(game.value)} disabled={is_first_game(game.value, @selected_games)}>
-              Up
+        <:right>
+          <LeftToRight>
+            <Button variant="secondary" on_click={@on_close}>
+              <:left_icon_slot><IconCloseSmall /></:left_icon_slot>
+              Close
             </Button>
-            <Button {:on_click, move_down_event(game.value)} disabled={is_last_game(game.value, @selected_games)}>
-              Down
+            <Button on_click={@on_submit} disabled={!@changeset.valid?}>
+              <:left_icon_slot><IconPencil /></:left_icon_slot>
+              Update
             </Button>
-          </Column>
-          <Column name="value" label="Game Value">
-            {game.value}
-          </Column>
-          <Column name="label" label="Game Label">
-            {game.label}
-          </Column>
-        </MoonTable>
-
+          </LeftToRight>
+        </:right>
+      </PullAside>
 
       </:content>
     </SlideOver>
     """
   end
 
-  def handle_event("single_row_click", %{"selected" => selected}, socket) do
-    {:noreply, assign(socket, selected: [selected])}
+  ##  Event Handlers for the Up and Down Buttons for Games
+  def handle_event("move_up:" <> index, _params, socket) do
+    %{changes: changes} = changeset = socket.assigns.changeset
+    index = String.to_integer(index)
+
+    game_ids = Map.get(changes, :game_ids, [])
+    category_ids = Map.get(changes, :category_ids, [])
+    name = Map.get(changes, :name, "")
+
+    updated_game_ids =
+      game_ids
+      |> List.replace_at(index, Enum.at(game_ids, index - 1))
+      |> List.replace_at(index - 1, Enum.at(game_ids, index))
+
+    # category_ids added
+    updated_changeset =
+      CreateOrUpdateCategoryGroup.changeset(changeset, %{
+        "game_ids" => updated_game_ids,
+        "category_ids" => category_ids,
+        "name" => name
+      })
+
+    send(self(), {"update_changeset", updated_changeset})
+    {:noreply, assign(socket, changeset: updated_changeset)}
   end
 
-  ##  Event Handlers for the Up and Down Buttons
-  def handle_event("move_up", %{"id" => game_id}, socket) do
-    IO.inspect(game_id, label: "Game ID in move_up_event")
-    index = Enum.find_index(socket.assigns.selected_games, &(&1.value == game_id))
-    IO.inspect(index, label: "Index of Game ID in move_up_event")
-    updated_games = swap_elements(socket.assigns.selected_games, index, index - 1)
-    {:noreply, assign(socket, selected_games: updated_games)}
+  def handle_event("move_down:" <> index, _params, socket) do
+    %{changes: changes} = changeset = socket.assigns.changeset
+    index = String.to_integer(index)
+
+    game_ids = Map.get(changes, :game_ids, [])
+    category_ids = Map.get(changes, :category_ids, [])
+    name = Map.get(changes, :name, "")
+
+    updated_game_ids =
+      game_ids
+      |> List.replace_at(index, Enum.at(game_ids, index + 1))
+      |> List.replace_at(index + 1, Enum.at(game_ids, index))
+
+    updated_changeset =
+      CreateOrUpdateCategoryGroup.changeset(changeset, %{
+        "game_ids" => updated_game_ids,
+        "category_ids" => category_ids,
+        "name" => name
+      })
+
+    send(self(), {"update_changeset", updated_changeset})
+    {:noreply, assign(socket, changeset: updated_changeset)}
   end
 
-  def handle_event("move_down", %{"id" => game_id}, socket) do
-    IO.inspect(game_id, label: "Game ID in move_down_event")
-    index = Enum.find_index(socket.assigns.selected_games, &(&1.value == game_id))
-    IO.inspect(index, label: "Index of Game ID in move_down_event")
-    updated_games = swap_elements(socket.assigns.selected_games, index, index + 1)
-    {:noreply, assign(socket, selected_games: updated_games)}
+  ##  Event Handlers for the Up and Down Buttons for Categories
+  def handle_event("move_up_category:" <> index, _params, socket) do
+    %{changes: changes} = changeset = socket.assigns.changeset
+    index = String.to_integer(index)
+
+    game_ids = Map.get(changes, :game_ids, [])
+    category_ids = Map.get(changes, :category_ids, [])
+    name = Map.get(changes, :name, "")
+
+    updated_category_ids =
+      category_ids
+      |> List.replace_at(index, Enum.at(category_ids, index - 1))
+      |> List.replace_at(index - 1, Enum.at(category_ids, index))
+
+    updated_changeset =
+      CreateOrUpdateCategoryGroup.changeset(changeset, %{
+        "game_ids" => game_ids,
+        "category_ids" => updated_category_ids,
+        "name" => name
+      })
+
+    send(self(), {"update_changeset", updated_changeset})
+    {:noreply, assign(socket, changeset: updated_changeset)}
   end
 
-  ## Disable the Up and Down Buttons for the First and Last Games
-  defp is_first_game(game_id, selected_games) do
-    game_id == hd(selected_games).value
+  def handle_event("move_down_category:" <> index, _params, socket) do
+    %{changes: changes} = changeset = socket.assigns.changeset
+    index = String.to_integer(index)
+
+    game_ids = Map.get(changes, :game_ids, [])
+    category_ids = Map.get(changes, :category_ids, [])
+    name = Map.get(changes, :name, "")
+
+    updated_category_ids =
+      category_ids
+      |> List.replace_at(index, Enum.at(category_ids, index + 1))
+      |> List.replace_at(index + 1, Enum.at(category_ids, index))
+
+    # game_ids added
+    updated_changeset =
+      CreateOrUpdateCategoryGroup.changeset(changeset, %{
+        "game_ids" => game_ids,
+        "category_ids" => updated_category_ids,
+        "name" => name
+      })
+
+    send(self(), {"update_changeset", updated_changeset})
+    {:noreply, assign(socket, changeset: updated_changeset)}
   end
 
-  defp is_last_game(game_id, selected_games) do
-    game_id == List.last(selected_games).value
-  end
-
-  defp swap_elements(list, index1, index2) do
-    [elem1, elem2] = [Enum.at(list, index1), Enum.at(list, index2)]
-    IO.inspect([elem1, elem2], label: "Elements to Swap in swap_elements")
-    list
-    |> List.replace_at(index1, elem2)
-    |> List.replace_at(index2, elem1)
-  end
-
+  ## Helper functions for the move up and down events
   defp move_up_event(game_id) do
-    # IO.inspect(game_id, label: "Game ID in move_up_event")
-    {"move_up", %{id: game_id}}
+    "move_up:#{game_id}"
   end
 
   defp move_down_event(game_id) do
-    # IO.inspect(game_id, label: "Game ID in move_down_event")
-    {"move_down", %{id: game_id}}
+    "move_down:#{game_id}"
   end
 
+  defp move_up_event_category(category_id) do
+    "move_up_category:#{category_id}"
+  end
 
-  # defp update_category_group_game_ids(socket, %{assigns: %{update_changeset: changeset, auth: auth, selected_games: selected_games}}) do
-  #   game_ids = Enum.map(selected_games, & &1.id)
-  #   changeset = Changeset.change(changeset, %{game_ids: game_ids})
-  #   case CreateOrUpdateCategoryGroup.update(changeset, auth) do
-  #     {:ok, _id} -> socket
-  #     {:error, changeset} -> assign(socket, update_changeset: changeset)
-  #   end
-  # end
+  defp move_down_event_category(category_id) do
+    "move_down_category:#{category_id}"
+  end
 end
